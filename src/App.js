8a662a47c77e4576d72abaffbfba4766eab3c646
Divyanshu Sahu <dsahu1997@gmail.com>
2019-07-30 20:50:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './assets/css/App.css';
+import SignUp from './components/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      ldsfds
+    <div className="wrapper">
+      <SignUp />
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { slide as SlideSignUp } from 'react-burger-menu';
+
+function SignUp() {
+  return (
+    <SlideSignUp right>
+      <p>SignUp</p>
+    </SlideSignUp>
+  )
+}
+
+export default SignUp;
