b0711b399e3b85f2c7ca8cd1b30264d071f548c8
Divyanshu Sahu <dsahu1997@gmail.com>
2019-08-02 01:20:29 +0530

    updated

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,36 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+
+import "../assets/css/Landing.css";
 
 class Landing extends Component {
+
   render() {
     return (
-      <div>
+      <div id="landing">
+        <section>
+          <div className="container">
+            <div className="center">
+              <div id="sell_progress_bar">
+                <span className="baricon orange"></span>
+                <span id="progress_1" className="line_progress"></span>
+                <span className="baricon orange"></span>
+                <span id="progress_2" className="line_progress"></span>
+                <span className="baricon orange"></span>
+              </div>
+              <div id="gadget_deatils">
+                
+              </div>
+              <div id="book_appointment">
 
+              </div>
+              <div id="sell_phone">
+
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    )
+    );
   }
 }
 
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,28 +1,79 @@
 import React, { Component } from "react";
 import logo from "../../assets/img/logo.svg";
+import "../../assets/css/Navbar.css";
 
 class Header extends Component {
-  render () {
+  render() {
     return (
       <header>
         <div className="navbar-fixed">
           <nav className="z-depth-0">
             <div className="nav-wrapper white">
-              <div className="container">
-                <a href="/" className="brand-logo">
+              <div className="navbar-container-width">
+                <a href="/" className="brand-logo left">
                   <img src={logo} alt="" />
                 </a>
-                <ul className="right hide-on-med-and-down">
-                  <li><a href="!#" className="grey-text text-darken-4">Location</a></li>
-                  <li><a href="!#" className="grey-text text-darken-4">Login</a></li>
+
+                <ul className="right hide-on-med-and-down navbar_components">
+                  <li>
+                    <a href="!#" className="red-text">
+                      HOME
+                    </a>
+                  </li>
+                  <li>
+                    <a href="!#" className="black-text">
+                      HOW IT WORKS
+                    </a>
+                  </li>
+                  <li>
+                    <a href="!#" className="black-text">
+                      CUSTOMER REVIEWS
+                    </a>
+                  </li>
+                  <li>
+                    <a href="!#" className="black-text">
+                      FAQ
+                    </a>
+                  </li>
+                  <li>
+                    <a href="!#" className="black-text">
+                      BLOG
+                    </a>
+                  </li>
+                  <li>
+                    <a href="!#" className="black-text">
+                      BECOME OUR PARTNER
+                    </a>
+                  </li>
+                  <li>
+                    <a href="!#" className="black-text">
+                      SERVICES
+                    </a>
+                  </li>
+                  <li style={{ borderLeft: "1px solid rgba(0,0,0,0.2)", 
+                               borderRight: "1px solid rgba(0,0,0,0.2)", 
+                               fontWeight: "400"
+                            }}>
+                    <a href="!#" className="black-text">
+                      <i className="material-icons left">location_on</i>
+                      New Delhi
+                      <i className="material-icons right">arrow_drop_down</i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="!#" className="blue-text">
+                      <i className="material-icons left">phone</i>
+                      1800-12345-2323
+                    </a>
+                  </li>
                 </ul>
               </div>
             </div>
           </nav>
         </div>
       </header>
-    )
-  }  
+    );
+  }
 }
 
 export default Header;
