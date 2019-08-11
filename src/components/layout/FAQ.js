635b4e1f7e030b216bbeb6d6a433514bf56fee82
Divyanshu Sahu <dsahu1997@gmail.com>
2019-08-11 18:08:07 +0530

    FAQ minor changes

--- src/components/layout/FAQ.js
+++ src/components/layout/FAQ.js
@@ -1,128 +1,142 @@
-import React from 'react'
+import React from "react";
+
+import "../../assets/css/FAQ.css";
 
 function FAQ() {
   return (
-    <div className="default_faq">
-      <ul className="collapsible expandable z-depth-0">
-        <li className="">
-          <div className="collapsible-header">
-            Which areas do you cover currently?
-          </div>
-          <div className="collapsible-body">
-            <span>
-              Currently we are operational in Delhi-NCR, Bangalore, Jaipur,
-              Pune, Lucknow, Ahmedabad, Hyderabad. We ae in the expansion
-              phase right now so you'll be seeing more stores soon.
-            </span>
-          </div>
-        </li>
-        <li>
-          <div className="collapsible-header">
-            What do you mean by Instant Cash?
-          </div>
-          <div className="collapsible-body">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </span>
-          </div>
-        </li>
-        <li>
-          <div className="collapsible-header">
-            How do you determine the price for my device?
-          </div>
-          <div className="collapsible-body">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </span>
-          </div>
-        </li>
-        <li>
-          <div className="collapsible-header">
-            Do you charge something for picking up my gadget?
-          </div>
-          <div className="collapsible-body">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </span>
-          </div>
-        </li>
-        <li>
-          <div className="collapsible-header">
-            What if my gadget is under warranty and I have lost the invoice?
-          </div>
-          <div className="collapsible-body">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </span>
-          </div>
-        </li>
-        <li>
-          <div className="collapsible-header">
-            I bought a phone from the USA and now I want to sell it. Will you
-            buy it and do you buy locked phones?
-          </div>
-          <div className="collapsible-body">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </span>
-          </div>
-        </li>
-      </ul>
+    <div id="default_faq">
+      <p className="faq-main-heading">Have Questions ?</p>
+      <p className="faq-paragraph grey-text text-darken-2">
+        If you have any questions about our preowned phone selling process or
+        want to know more about reCash. Call us on 0987654321.
+      </p>
+      <p className="faq-mini-paragraph grey-text text-darken-1">
+        Meanwhile, you can get answers to the frequently asked questions here
+      </p>
+      <div className="row">
+        <div className="col s12 m8">
+          <ul className="collapsible expandable z-depth-0">
+            <li className="">
+              <div className="collapsible-header">
+                Which areas do you cover currently?
+              </div>
+              <div className="collapsible-body">
+                <span>
+                  Currently we are operational in Delhi-NCR, Bangalore, Jaipur,
+                  Pune, Lucknow, Ahmedabad, Hyderabad. We ae in the expansion
+                  phase right now so you'll be seeing more stores soon.
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="collapsible-header">
+                What do you mean by Instant Cash?
+              </div>
+              <div className="collapsible-body">
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="collapsible-header">
+                How do you determine the price for my device?
+              </div>
+              <div className="collapsible-body">
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="collapsible-header">
+                Do you charge something for picking up my gadget?
+              </div>
+              <div className="collapsible-body">
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="collapsible-header">
+                What if my gadget is under warranty and I have lost the invoice?
+              </div>
+              <div className="collapsible-body">
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className="collapsible-header">
+                I bought a phone from the USA and now I want to sell it. Will
+                you buy it and do you buy locked phones?
+              </div>
+              <div className="collapsible-body">
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi
+                  ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                  veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
