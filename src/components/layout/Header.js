3d2cde98426f646163bb8d697f98170fa37534d9
Divyanshu Sahu <dsahu1997@gmail.com>
2019-08-15 13:01:47 +0530

    updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Headroom from "react-headroom";
 //import axios from "axios";
 import { signupToggle } from "../../actions/signupToggleActions";
 
@@ -34,89 +35,93 @@ class Header extends Component {
 
   render() {
     return (
-      <header>
-        <div className="navbar-fixed">
-          <nav className="z-depth-0">
-            <div className="nav-wrapper white">
-              <div className="navbar-container-width">
-                <a href="/" className="brand-logo left">
-                  <img src={logo} alt="" />
-                </a>
+      <Headroom>
+        <header>
+          <div className="">
+            <nav className="z-depth-0">
+              <div className="nav-wrapper white">
+                <div className="navbar-container-width">
+                  <a href="/" className="brand-logo left">
+                    <img src={logo} alt="" />
+                  </a>
 
-                <ul className="right hide-on-med-and-down navbar_components">
-                  <li>
-                    <Link to="/">
-                      <span className="red-text">HOME</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#how-it-works">
-                      <span className="black-text">HOW IT WORKS</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#customers-review">
-                      <span className="black-text">CUSTOMER REVIEWS</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#default_faq">
-                      <span className="black-text">FAQ</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/blog">
-                      <span className="black-text">BLOG</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/become-a-partner">
-                      <span className="black-text">BECOME OUR PARTNER</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="#!">
-                      <span className="black-text">SERVICES</span>
-                    </Link>
-                  </li>
-                  <li
-                    style={{
-                      borderLeft: "1px solid rgba(0,0,0,0.2)",
-                      borderRight: "1px solid rgba(0,0,0,0.2)",
-                      fontWeight: "400"
-                    }}
-                  >
-                    <Link to="#!">
-                      <span className="black-text">
-                        <i className="material-icons left">location_on</i>
-                        New Delhi
-                        <i className="material-icons right">arrow_drop_down</i>
-                      </span>
-                    </Link>
-                  </li>
-                  <li style={{ borderRight: "1px solid rgba(0,0,0,0.2)" }}>
-                    <Link to="#!">
-                      <span className="blue-text">
-                        <i className="material-icons left">phone</i>
-                        1800-12345-2323
-                      </span>
-                    </Link>
-                  </li>
-                  <li onClick={() => this.props.signupToggle(false)}>
-                    <Link to="#!">
-                      <span className="black-text">
-                        {this.state.isAuthenticated
-                          ? this.state.user
-                          : "SIGN UP"}
-                      </span>
-                    </Link>
-                  </li>
-                </ul>
+                  <ul className="right hide-on-med-and-down navbar_components">
+                    <li>
+                      <Link to="/">
+                        <span className="red-text">HOME</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#how-it-works">
+                        <span className="black-text">HOW IT WORKS</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#customers-review">
+                        <span className="black-text">CUSTOMER REVIEWS</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#default_faq">
+                        <span className="black-text">FAQ</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/blog">
+                        <span className="black-text">BLOG</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/become-a-partner">
+                        <span className="black-text">BECOME OUR PARTNER</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="#!">
+                        <span className="black-text">SERVICES</span>
+                      </Link>
+                    </li>
+                    <li
+                      style={{
+                        borderLeft: "1px solid rgba(0,0,0,0.2)",
+                        borderRight: "1px solid rgba(0,0,0,0.2)",
+                        fontWeight: "400"
+                      }}
+                    >
+                      <Link to="#!">
+                        <span className="black-text">
+                          <i className="material-icons left">location_on</i>
+                          New Delhi
+                          <i className="material-icons right">
+                            arrow_drop_down
+                          </i>
+                        </span>
+                      </Link>
+                    </li>
+                    <li style={{ borderRight: "1px solid rgba(0,0,0,0.2)" }}>
+                      <Link to="#!">
+                        <span className="blue-text">
+                          <i className="material-icons left">phone</i>
+                          1800-12345-2323
+                        </span>
+                      </Link>
+                    </li>
+                    <li onClick={() => this.props.signupToggle(false)}>
+                      <Link to="#!">
+                        <span className="black-text">
+                          {this.state.isAuthenticated
+                            ? this.state.user
+                            : "SIGN UP"}
+                        </span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
-          </nav>
-        </div>
-      </header>
+            </nav>
+          </div>
+        </header>
+      </Headroom>
     );
   }
 }
